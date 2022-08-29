bd7583f071c19b86acecce934ac76720b50a105e
tmosharaf <mousharaftanvir@users.noreply.github.com>
2022-08-30 00:53:07 +0600

    Day1: Played with variables

---
+++ day1/var.js
@@ -0,0 +1,14 @@
+const myFirstVar = "This is my First Variable";
+
+const anotherVar = 2;
+
+
+console.log(myFirstVar);
+console.log(anotherVar);
+
+//I can declare a variable first & then assign a value into it.
+let anotherAnotherVar;
+
+anotherAnotherVar = "Value set into it after declaration.";
+
+console.log(anotherAnotherVar)
